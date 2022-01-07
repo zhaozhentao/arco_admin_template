c314567e16c757224af1cb487a5d39f023f0489a
zhaozhentao <344696734@qq.com>
2022-01-07 10:20:36 +0800

    🎨narbar

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="menu-demo">
+    <div class="layout-navbar">
+      <div class="navbar">
+
+      </div>
+    </div>
+
     <a-menu
         :style="{ width: '200px', height: '100%' }"
         :default-open-keys="['0', '1', '2']"
@@ -46,8 +52,27 @@ export default {
 </script>
 
 <style scoped>
+.layout-navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  width: 100%;
+  min-width: 1100px;
+  height: 60px;
+}
+
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+  background-color: var(--color-bg-2);
+  border-bottom: 1px solid var(--color-border);
+}
+
 .menu-demo {
   box-sizing: border-box;
+  padding-top: 60px;
   width: 100%;
   height: 100%;
 }
