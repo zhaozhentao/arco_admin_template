08a4bda076de5f3be08cecfd4d6f8346b7d5be2b
zhaozhentao <344696734@qq.com>
2022-01-07 09:54:07 +0800

    🎨简化文件引用路径

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      '@': resolve('src')
+    }
+  },
   plugins: [vue()]
 })
