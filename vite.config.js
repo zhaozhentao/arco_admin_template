4ae3a427718fa4436efe6a2aa5f69c50484c690c
zhaozhentao <qq344696734@gmail.com>
2022-01-10 15:34:55 +0800

    🎨format

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteMockServe } from "vite-plugin-mock"
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig(({command}) => {
