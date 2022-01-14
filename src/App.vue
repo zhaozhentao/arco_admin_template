f5e8742efb4768a677d6bedd07d5cfbf376833a8
zhaozhentao <qq344696734@gmail.com>
2022-01-14 16:00:09 +0800

    🎨clean code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
 <template>
   <router-view />
 </template>
-
-<style>
-html {
-  height: 100%;
-}
-
-body {
-  background-color: var(--color-neutral-2);
-  height: 100%;
-}
-
-#app {
-  font-family: Inter, -apple-system, BlinkMacSystemFont, PingFang SC, Hiragino Sans GB, noto sans, Microsoft YaHei, Helvetica Neue, Helvetica, Arial, sans-serif;;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  height: 100%;
-}
-</style>
