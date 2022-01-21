a759a8cd75ae511028da466fe8368f4f6ec8875f
zhaozhentao <qq344696734@gmail.com>
2022-01-21 09:52:40 +0800

    🎨可以显示高亮

--- src/views/donate/index.vue
+++ src/views/donate/index.vue
@@ -1,3 +1,47 @@
 <template>
-
+  <div>
+    <textarea></textarea>
+  </div>
 </template>
+
+<script>
+import '@/simplemde.min'
+import hljs from 'highlight.js/lib/core'
+import php from 'highlight.js/lib/languages/php'
+import { marked } from 'marked'
+
+export default {
+  mounted() {
+    hljs.initHighlightingOnLoad()
+    hljs.registerLanguage('php', php);
+
+    let s = new SimpleMDE({
+      renderingConfig: {
+        singleLineBreaks: false,
+        codeSyntaxHighlighting: true,
+      },
+      previewRender: (plainText) => {
+        return marked(plainText, {
+          langPrefix: 'hljs ',
+          renderer: new marked.Renderer(),
+          gfm: true,
+          pedantic: false,
+          sanitize: false,
+          tables: true,
+          breaks: true,
+          smartLists: true,
+          smartypants: true,
+          highlight: function (code) {
+            return hljs.highlightAuto(code).value;
+          }
+        });
+      }
+    })
+  }
+}
+</script>
+
+<style lang="css">
+@import "../../styles/simplemde.min.css";
+@import "../../../node_modules/highlight.js/styles/monokai-sublime.css";
+</style>
