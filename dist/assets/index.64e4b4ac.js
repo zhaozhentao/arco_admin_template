6cb2c2ac7ddc1168c70a109520d0909135de9e7f
zhaozhentao <qq344696734@gmail.com>
2022-08-10 15:09:23 +0800

    更新dist

---
+++ dist/assets/index.64e4b4ac.js
@@ -0,0 +1,1 @@
+const e={__name:"index",setup(t){return window.location.href="https://arco.design/vue/docs/start",()=>{}}};export{e as default};
--- dist/assets/index.ffce9194.js
+++
@@ -1,1 +0,0 @@
-const s={setup(t){return window.location.href="https://arco.design/vue/docs/start",()=>{}}};export{s as default};
